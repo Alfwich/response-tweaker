eb052077f530192a6f7a5fc0d676700c78a04bd5
Arthur Wuterich <arthur.wuterich@mlb.com>
2016-11-17 14:03:53 -0800

    Init

---
+++ response-tweaker/response-tweaker/JObjectViewer.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Newtonsoft.Json.Linq;
+using response_tweaker.Annotations;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace response_tweaker
+{
+    public sealed partial class JObjectViewer : UserControl
+    {
+        public static readonly DependencyProperty SourceJObjectProperty = DependencyProperty.Register("SourceJObject", typeof(object), typeof(JObjectViewer), null);
+
+        public JObjectViewerViewModel ViewModel { get; set; }
+        private object _currentLevel;
+        private readonly Stack<object> _levels = new Stack<object>();
+
+        public JObjectViewer()
+        {
+            ViewModel = new JObjectViewerViewModel();
+            InitializeComponent();
+        }
+
+        private void PushLevelAndLoadNewCurrent(object level)
+        {
+            if (_currentLevel == level)
+            {
+                return;
+            }
+
+            _levels.Push(_currentLevel);
+            UpdateCurrentLevelView(level);
+            PostLevelStackMutate();
+        }
+
+        private void PopLevelAndLoadNewCurrent()
+        {
+            UpdateCurrentLevelView(_levels.Peek());
+            _levels.Pop();
+            PostLevelStackMutate();
+        }
+
+        private void PostLevelStackMutate()
+        {
+            ViewModel.BackVisibility = _levels.Count > 0;
+            if (_currentLevel != null)
+            {
+                var jObject = _currentLevel as JObject;
+                if (jObject != null)
+                {
+                    ViewModel.CurrentPath = jObject.Path;
+                    return;
+                }
+
+                var jArray = _currentLevel as JArray;
+                if (jArray != null)
+                {
+                    ViewModel.CurrentPath = jArray.Path;
+                }
+
+            }
+        }
+
+        private bool UpdateCurrentLevelView(object level)
+        {
+            if (_currentLevel == level)
+            {
+                return false;
+            }
+
+            var jObject = level as JObject;
+            if (jObject != null)
+            {
+                _currentLevel = level;
+                ViewModel.JObjectCurrentListing.Clear();
+                UpdateCurrentLevelJObject(jObject);
+                return true;
+            }
+
+            var jArray = level as JArray;
+            if (jArray != null)
+            {
+                _currentLevel = level;
+                ViewModel.JObjectCurrentListing.Clear();
+                UpdateCurrentLevelJArray(jArray);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void UpdateCurrentLevelJObject(JObject level)
+        {
+            foreach (var keyValuePair in level)
+            {
+                ViewModel.JObjectCurrentListing.Add(new JObjectRow
+                {
+                    Parent = level,
+                    Key = keyValuePair.Key,
+                    Value = keyValuePair.Value,
+                    Label = $"{keyValuePair.Key}: {keyValuePair.Value}",
+                    ClickEnabled = keyValuePair.Value is JObject || keyValuePair.Value is JArray
+                });
+            }
+        }
+
+        private void UpdateCurrentLevelJArray(JArray level, int startIndex = 0)
+        {
+            foreach (var token in level.Children())
+            {
+                var key = startIndex++.ToString();
+                ViewModel.JObjectCurrentListing.Add(new JObjectRow
+                {
+                    Parent = level,
+                    Key = key,
+                    Value = token,
+                    Label = $"{key}: {token}",
+                    ClickEnabled = token is JObject || token is JArray
+                });
+            }
+        }
+
+        public object SourceJObject
+        {
+            get
+            {
+                return GetValue(SourceJObjectProperty) as JObject;
+            }
+            set
+            {
+                SetValue(SourceJObjectProperty, value);
+                UpdateCurrentLevelView(value);
+            }
+        }
+
+        private void ObjectLevelViewer_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var objectRow = e.ClickedItem as JObjectRow;
+            if (objectRow != null && objectRow.ClickEnabled)
+            {
+                PushLevelAndLoadNewCurrent(objectRow.Value);
+            }
+        }
+
+        private void BackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            PopLevelAndLoadNewCurrent();
+        }
+    }
+
+    public class JObjectRow
+    {
+        public object Parent { get; set; }
+        public string Label { get; set; }
+        public string Key { get; set; }
+        public object Value { get; set; }
+        public bool ClickEnabled { get; set; }
+    }
+
+    public class JObjectViewerViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<JObjectRow> JObjectCurrentListing { get; set; } = new ObservableCollection<JObjectRow>();
+
+        private bool _backVisibility;
+        public bool BackVisibility
+        {
+            get
+            {
+                return _backVisibility;
+            }
+            set
+            {
+                _backVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _currentPath;
+
+        public string CurrentPath
+        {
+            get
+            {
+                return _currentPath;
+            }
+            set
+            {
+                _currentPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ response-tweaker/response-tweaker/MainPage.xaml.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.Storage.Pickers;
+using Newtonsoft.Json.Linq;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace response_tweaker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPageViewModel ViewModel { get; set; }
+        private RequestFileContents _requestFileContents = null;
+        public MainPage()
+        {
+            ViewModel = new MainPageViewModel();
+            InitializeComponent();
+        }
+
+        private FileOpenPicker GetFilePicker()
+        {
+            var picker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.List,
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+            };
+            picker.FileTypeFilter.Add(".txt");
+
+            return picker;
+        }
+
+        private async Task<StorageFile> GetFile()
+        {
+            var picker = GetFilePicker();
+            return await picker.PickSingleFileAsync();
+        }
+
+        private async Task SetOpenedFileState(StorageFile file)
+        {
+            ViewModel.SaveEnabled = true;
+            ViewModel.ParseEnabled = true;
+            ViewModel.FileNamePath = file.Path;
+            ViewModel.SavedFileNamePath = string.Empty;
+            _requestFileContents = await RequestFileContents.ReadFileContents(file);
+            ViewModel.OriginalFileContent = _requestFileContents.Payload;
+        }
+
+        private void SetClosedOrFailedFileState()
+        {
+            ViewModel.SaveEnabled = false;
+            ViewModel.ParseEnabled = false;
+            ViewModel.FileNamePath = string.Empty;
+            ViewModel.OriginalFileContent = string.Empty;
+            ViewModel.SavedFileNamePath = string.Empty;
+        }
+
+        private async void LoadFileButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var file = await GetFile();
+            if (file != null)
+            {
+                await SetOpenedFileState(file);
+            }
+            else
+            {
+                SetClosedOrFailedFileState();
+            }
+        }
+
+        private void LoadUrlButton_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ParseData_OnClick(object sender, RoutedEventArgs e)
+        {
+            ViewModel.SourceObject = _requestFileContents.GetPayloadObject();
+            ViewModel.ParseEnabled = false;
+        }
+    }
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private object _sourceObject;
+
+        public object SourceObject
+        {
+            get
+            {
+                return _sourceObject;
+            }
+            set
+            {
+                _sourceObject = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _fileNamePath;
+        public string FileNamePath
+        {
+            get
+            {
+                return _fileNamePath;
+            }
+            set
+            {
+                _fileNamePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _savedFileNamePath;
+        public string SavedFileNamePath
+        {
+            get
+            {
+                return _savedFileNamePath;
+            }
+            set
+            {
+                _savedFileNamePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _originalFileContent;
+        public string OriginalFileContent
+        {
+            get
+            {
+                return _originalFileContent;
+            }
+            set
+            {
+                _originalFileContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _saveEnabled;
+        public bool SaveEnabled
+        {
+            get
+            {
+                return _saveEnabled;
+            }
+            set
+            {
+                _saveEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _parseEnabled;
+        public bool ParseEnabled
+        {
+            get
+            {
+                return _parseEnabled;
+            }
+            set
+            {
+                _parseEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public class RequestFileContents
+    {
+        public static async Task<RequestFileContents> ReadFileContents(StorageFile file)
+        {
+            var resultRequestFileContents = new RequestFileContents();
+            var hasHitPayload = false;
+
+            using (var inputStream = await file.OpenReadAsync())
+            using (var classicsStream = inputStream.AsStreamForRead())
+            using (var streamReader = new StreamReader(classicsStream))
+            {
+                while (streamReader.Peek() > 0)
+                {
+                    var line = $"{streamReader.ReadLine()}\n";
+                    if (line == "\n")
+                    {
+                        hasHitPayload = true;
+                        continue;
+                    }
+
+                    if (hasHitPayload)
+                    {
+                        resultRequestFileContents.Payload += line;
+                    }
+                    else
+                    {
+                        resultRequestFileContents.Headers += line;
+                    }
+                }
+            }
+
+            return resultRequestFileContents;
+        }
+
+        public string Headers { get; private set; } = string.Empty;
+        public string Payload { get; private set; } = string.Empty;
+
+        private JObject _deserializedObject;
+
+        public object GetPayloadObject()
+        {
+            if (_deserializedObject != null)
+            {
+                return _deserializedObject;
+            }
+
+            try
+            {
+                _deserializedObject = JObject.Parse(Payload);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to deserialize object: {Payload}\nmessage: {ex.Message}");
+                _deserializedObject = null;
+            }
+
+            return _deserializedObject;
+        }
+
+    }
+}
