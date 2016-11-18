940e785f363e646556a5a977071124610cee367b
Arthur Wuterich <arthur.wuterich@mlb.com>
2016-11-17 18:11:40 -0800

    Updated

--- response-tweaker/response-tweaker/JObjectViewer.xaml.cs
+++ response-tweaker/response-tweaker/JObjectViewer.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks.Dataflow;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Newtonsoft.Json.Linq;
 using response_tweaker.Annotations;
 
@@ -102,9 +106,11 @@ namespace response_tweaker
                 {
                     Parent = level,
                     Key = keyValuePair.Key,
+                    KeyLabel = keyValuePair.Key,
                     Value = keyValuePair.Value,
-                    Label = $"{keyValuePair.Key}: {keyValuePair.Value}",
-                    ClickEnabled = keyValuePair.Value is JObject || keyValuePair.Value is JArray
+                    ValueLabel = keyValuePair.Value.ToString(),
+                    ClickEnabled = keyValuePair.Value is JObject || keyValuePair.Value is JArray,
+                    EditAllowed = !(keyValuePair.Value is JObject || keyValuePair.Value is JArray)
                 });
             }
         }
@@ -118,9 +124,11 @@ namespace response_tweaker
                 {
                     Parent = level,
                     Key = key,
+                    KeyLabel = key,
                     Value = token,
-                    Label = $"{key}: {token}",
-                    ClickEnabled = token is JObject || token is JArray
+                    ValueLabel = token.ToString(),
+                    ClickEnabled = token is JObject || token is JArray,
+                    EditAllowed = !(token is JObject || token is JArray)
                 });
             }
         }
@@ -138,12 +146,11 @@ namespace response_tweaker
             }
         }
 
-        private void ObjectLevelViewer_OnItemClick(object sender, ItemClickEventArgs e)
+        private void JObjectRowClicked(JObjectRow row)
         {
-            var objectRow = e.ClickedItem as JObjectRow;
-            if (objectRow != null && objectRow.ClickEnabled)
+            if (row != null && row.ClickEnabled)
             {
-                PushLevelAndLoadNewCurrent(objectRow.Value);
+                PushLevelAndLoadNewCurrent(row.Value);
             }
         }
 
@@ -151,15 +158,143 @@ namespace response_tweaker
         {
             PopLevelAndLoadNewCurrent();
         }
+
+        private void ObjectLevelViewer_OnKeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case VirtualKey.Enter:
+                    var listViewItem = e.OriginalSource as ListViewItem;
+                    JObjectRowClicked(listViewItem?.Content as JObjectRow);
+                    break;
+            }
+
+        }
+
+        private void ObjectLevelViewer_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            JObjectRowClicked(e.ClickedItem as JObjectRow);
+        }
     }
 
-    public class JObjectRow
+    public class JObjectRow : INotifyPropertyChanged
     {
         public object Parent { get; set; }
-        public string Label { get; set; }
+        public string KeyLabel { get; set; }
         public string Key { get; set; }
-        public object Value { get; set; }
+        public JToken Value { get; set; }
+
+        private string _valueLabel;
+        public string ValueLabel
+        {
+            get
+            {
+                return _valueLabel;
+            }
+            set
+            {
+                _valueLabel = value;
+                OnPropertyChanged();
+            }
+        }
         public bool ClickEnabled { get; set; }
+        private bool _editEnabled;
+        public bool EditEnabled
+        {
+            get { return _editEnabled; }
+            set
+            {
+                _editEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _editButtonLabel = "Edit";
+
+        public string EditButtonLabel
+        {
+            get
+            {
+                return _editButtonLabel;
+            }
+            set
+            {
+                _editButtonLabel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool EditAllowed { get; set; }
+
+        public Visibility EditFeaturesVisibility => EditAllowed ? Visibility.Visible : Visibility.Collapsed;
+
+        private Visibility _valueVisible = Visibility.Visible;
+        public Visibility ValueVisibility
+        {
+            get
+            {
+                return _valueVisible;
+            }
+            set
+            {
+                _valueVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Visibility _editVisible = Visibility.Collapsed;
+        public Visibility EditVisibility
+        {
+            get
+            {
+                return _editVisible;
+            }
+            set
+            {
+                _editVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void EditRequested(object sender, object e)
+        {
+            if (EditAllowed)
+            {
+                // Update
+                if (EditEnabled)
+                {
+                    var btn = sender as Button;
+                    var stackPanel = btn?.Parent as StackPanel;
+                    var newValue = string.Empty;
+                    foreach (var child in stackPanel?.Children)
+                    {
+                        var childTextBox = child as TextBox;
+                        if (childTextBox?.Name == "NewValue")
+                        {
+                            newValue = childTextBox.Text;
+                        }
+                    }
+
+                    if (newValue != string.Empty)
+                    {
+                        ValueLabel = newValue;
+                        Value = newValue;
+                    }
+                }
+
+                EditEnabled = !EditEnabled;
+                EditVisibility = EditEnabled ? Visibility.Visible : Visibility.Collapsed;
+                ValueVisibility = EditEnabled ? Visibility.Collapsed : Visibility.Visible;
+                EditButtonLabel = EditEnabled ? "Save" : "Edit";
+            }
+        }
     }
 
     public class JObjectViewerViewModel : INotifyPropertyChanged
